cb3fd7edab68bc46ecfaad36ac1d6953f10566f4
Dominik Pietrzyca <defres15@gmail.com>
2022-02-25 13:28:13 +0100

    fix netlify form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -18,6 +18,7 @@ export default function Form() {
       id="form"
       name="contact"
       action="POST"
+      method="POST"
       data-netlify="true"
       // onSubmit={handleSubmit}
     >
